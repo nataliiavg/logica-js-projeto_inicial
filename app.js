c6e3821003056f026dc659c504ecc384e94aba69
Natalia Graciano <nataliavgraciano@gmail.com>
2024-01-17 19:50:28 -0300

    Aula 02 - Condicionais e Concatenação

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = 9;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10: ');
 
+// se chute for igual ao número secreto
 if (chute == numeroSecreto){
-    console.log('Correto! Você descobriu o número secreto (5)');
+    alert(`Correto! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    console.log('Número secreto incorreto!')
+    alert('Você errou :(');
 }
