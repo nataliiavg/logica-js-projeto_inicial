1b019516308db12ad5e2556ba6c8c6e940005408
Natalia Graciano <nataliavgraciano@gmail.com>
2024-01-18 20:49:24 -0300

    Aula 04 - Boas Práticas de Programação

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 alert("Boas vindas ao jogo do número secreto!");
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao número secreto
 while (chute != numeroSecreto) {
-  chute = prompt("Escolha um número entre 1 e 10: ");
+  chute = prompt("Escolha um número entre 1 e 100");
   // se chute for igual ao número secreto
   if (chute == numeroSecreto) {
-    alert(`Correto! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa(s)`);
+    break;
   } else {
     if (chute > numeroSecreto) {
       alert(`O número secreto é menor que ${chute}`);
     } else {
-      alert(`O número secreto é maior que ${chute} `);
+      alert(`O número secreto é maior que ${chute}`);
     }
     // tentativas = tentativas + 1
-    tentativas ++;
+    tentativas++;
   }
 }
+
+// operador ternário
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(
+  `Correto! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`
+);
